3ac420ca2b198fb2d4b97ad542224063f7df22cd
abdulaziz <116705391+Azizkubanychbek@users.noreply.github.com>
2023-03-17 22:20:55 +0600

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,25 +1,30 @@
-﻿#include <iostream>
+#include <iostream>
 
 #define MODE 1
+#if MODE==1
+void add(int j, int b)
+{
+	std::cout << j + b;
+
+}
+#endif
 int main(int argc, char** argv) {
 #ifndef MODE
 	std::cout << "Where is MODE?";
 #endif // !MODE
+
 #ifdef MODE
 #if MODE==0
 	std::cout << "Работаю в режиме тренировки";
 #elif MODE==1
 	int j = 9;
 	int b = 0;
-	int add(int j,int b);
-	{
-		int l = j + b;
-		return l;
-	}
+
 	std::cout << "Работаю в боевом режиме\n";
 	std::cout << "Ввeдите два числа через enter\n";
 	std::cin >> b >> j;
-	std::cout << "Сумма равна " << add(b,j);
+	std::cout << "Сумма равна ";
+	add(b,j);
 	
 #else
 	std::sout << "Try smt better";
